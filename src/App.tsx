094239cf357abeac136b102bcc5466dd3267fcf5
Personal - Stakshi <personal-stakshi@Stakshis-MacBook-Air.local>
2025-04-13 14:18:06 +0530

    chore: add vercel speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Footer } from './components/Footer';
 import { ThemeProvider } from './components/ThemeProvider';
 import { ThemeToggle } from './components/ThemeToggle';
@@ -14,27 +15,19 @@ function App() {
               <span className="text-2xl"> 🐰🥕</span>
             </div>
             <div className="flex gap-2 text-2xl">
-              {/* Cute decorative elements */}
               <ThemeToggle />
-              {/* <span>🌸</span>
-            <span>🐰</span>
-            <span>🥕</span> */}
-
             </div>
           </div>
         </header>
-        {/* <div className="absolute top-20 left-10 text-4xl animate-bounce">🌸</div> */}
-        {/* <div className="absolute top-40 right-10 text-4xl animate-bounce delay-100">🥕</div> */}
         <main className="container mx-auto flex flex-col items-center justify-center mt-20 relative p-4">
           <div className="absolute -top-16 left-1/2 transform -translate-x-1/2 text-center text-[#FF99B4] text-sm">
             hop hop... type away! 🐰
           </div>
-
           <TypingTest />
         </main>
-
         <Footer />
       </div>
+      <SpeedInsights />
     </ThemeProvider>
 
   );
